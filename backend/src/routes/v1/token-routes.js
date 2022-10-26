92e9d709386e127e0befe80ff46ff4d05e06e168
topdev <topdev920123@gmail.com>
2022-10-26 05:22:31 -0700

    feat: integration token path list

--- backend/src/routes/v1/token-routes.js
+++ backend/src/routes/v1/token-routes.js
@@ -4,5 +4,8 @@ const tokenController = require('../../controllers/v1/token-controller');
 const router = express.Router();
 
 router.get('/get_tokens/:chainId/:address', tokenController.getTokens);
+router.get('/tokenList', tokenController.tokenList);
+router.get('/rateList', tokenController.rateList);
+router.get('/tokenPathList', tokenController.tokenPathList);
 
 module.exports = router;
